74669dcb1d32c07ace0d943bdfdf7be52eb6b32a
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2024-02-24 11:01:18 +0100

    implemented database fetch

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import PocketBase from "pocketbase";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Index page</h1>
-    </main>
-  );
+const pb = new PocketBase("https://pocketbase.funkysundays.com");
+
+async function getData() {
+    return await pb.collection("events").getList(1, 15, {
+        filter: "hidden != true",
+    });
+}
+
+export default async function Home() {
+    const data = await getData();
+    return (
+        <main className={styles.main}>
+            <h1>Index page</h1>
+            {data.items.map((i) => (
+                <article key={i.id}>
+                    <p>{i.name}</p>
+                </article>
+            ))}
+        </main>
+    );
 }
